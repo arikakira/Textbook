68a82d7753f7100264b6c9c0145376ba5a503ae7
arikakira <samanthal242@nycstudents.net>
2025-04-10 20:55:52 +0000

    Update

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Textbook bio2015 = new Textbook("Biology", 49.75, 2);       // test case 1
+        Textbook bio2019 = new Textbook("Biology", 39.75, 3);       // test case 2
+        System.out.println(bio2019.getEdition());       // test case 3
+        
+    }
+}
--- Textbook.java
+++ Textbook.java
@@ -1,5 +1,16 @@
 public class Textbook extends Book {
     private int edition;
 
-    
+    public Textbook(String title, double price, int ed) {
+        super(title, price);
+        edition = ed;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+
+//    public String getBookInfo() {
+        
+//    }
 }
